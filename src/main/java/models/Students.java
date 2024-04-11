eb6c88094bbb1782fb6adfb4815f0f681c472ee4
Shaily Modi <shailykunal@gmail.com>
2024-04-11 16:40:01 -0400

    completed Students class

---
+++ src/main/java/models/Students.java
@@ -0,0 +1,70 @@
+package models;
+
+public class Students {
+    private Integer id;
+    private String first_name;
+    private String last_name;
+    private Integer age;
+    private Integer height;
+    private Integer weight;
+
+    public Students() {
+    }
+
+    public Students(Integer id, String first_name, String last_name, Integer age, Integer height, Integer weight) {
+        this.id = id;
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.age = age;
+        this.height = height;
+        this.weight = weight;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Integer getHeight() {
+        return height;
+    }
+
+    public void setHeight(Integer height) {
+        this.height = height;
+    }
+
+    public Integer getWeight() {
+        return weight;
+    }
+
+    public void setWeight(Integer weight) {
+        this.weight = weight;
+    }
+}
