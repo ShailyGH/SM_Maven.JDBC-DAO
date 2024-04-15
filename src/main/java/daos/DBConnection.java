b71d08d4dc8556f102d7a8fad558d40f3f462090
Shaily Modi <shailykunal@gmail.com>
2024-04-15 13:12:03 -0400

    App class testing completed

--- src/main/java/daos/DBConnection.java
+++ src/main/java/daos/DBConnection.java
@@ -22,30 +22,7 @@ public class DBConnection
 
     public static void main(String[] args) {
 
-//        try {
-            Connection connection = DBConnection.getConnection();
-            //Statement stmt = connection.createStatement();
-
-//        String sql1 = "DROP DATABASE STUDENTS_DB ";
-//        stmt.executeUpdate(sql1);
-
-//            String sql2 = "CREATE DATABASE IF NOT EXISTS STUDENTS_DB ";
-//            stmt.executeUpdate(sql2);
-//
-//            String sql3 = "CREATE TABLE IF NOT EXISTS STUDENTS_DB.students " +
-//                    "(id INTEGER not NULL, " +
-//                    " first_name VARCHAR(255), " +
-//                    " last_name VARCHAR(255), " +
-//                    " age INTEGER, " +
-//                    " height INTEGER, " +
-//                    " weight INTEGER, " +
-//                    " PRIMARY KEY (id))";
-//            stmt.executeUpdate(sql3);
-        //}
-//        catch (SQLException e)
-//        {
-//            throw new RuntimeException("Error with SQL queries", e);
-//        }
+        Connection connection = DBConnection.getConnection();
     }
 
 }
